3d8a66db2cf43ffd91abd0e981ec902777afec4a
Your Name <you@example.com>
2023-02-11 04:37:27 +0000

    dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
 import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 
 function App() {
-  return <Box width={100} height={100} border={1}></Box>;
+  return <Grid container justifyContent="center">
+    <Grid item xs={12} sm={11} md={10} lg={9} xl={8} display="flex" alignItems="center" height="100vh" p={1}>
+      <Box border={1} height="98%" width="100%">
+      </Box>
+    </Grid>
+  </Grid>;
 }
 
 export default App;
